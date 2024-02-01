babf596b127746b80bb6ce63feda6be54eb5eeb2
x0archie <andri@x0swap.com>
2024-02-01 12:47:44 +0000

    fixing

--- index.js
+++ index.js
@@ -4,18 +4,23 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const fileUpload = require('express-fileupload')
 const fs = require('fs')
-const projectId = '2JRgSlpcFAFTDTvmS9ntQqfFkUZ';
-const projectSecret = '29ffe1fe5dee36c49c82a6ad045c3259';
-const auth =
-    'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
-const ipfs = new ipfsClient({
-    host: 'ipfs.infura.io',
-    port: 5001,
-    protocol: 'https',
-    headers: {
-        authorization: auth,
-    },
-})
+
+const pinataSDK = require("@pinata/sdk");
+const pinata = new pinataSDK({ pinataJWTKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiIyYWQ0YzczYS02OTk1LTQ2OGItOWQ2MC1hNzVhMjBjMDg1ZTciLCJlbWFpbCI6ImtyaXNkaWFudG8uYW5kcmlAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInBpbl9wb2xpY3kiOnsicmVnaW9ucyI6W3siaWQiOiJGUkExIiwiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjF9LHsiaWQiOiJOWUMxIiwiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjF9XSwidmVyc2lvbiI6MX0sIm1mYV9lbmFibGVkIjpmYWxzZSwic3RhdHVzIjoiQUNUSVZFIn0sImF1dGhlbnRpY2F0aW9uVHlwZSI6InNjb3BlZEtleSIsInNjb3BlZEtleUtleSI6IjFiNGE5YzcxZDE3NTA0ODNjNTA1Iiwic2NvcGVkS2V5U2VjcmV0IjoiY2FlNjIyYzM4YWRhOWRjN2MxNGNkYzk5MDU1MWFlMjIzOTliZDRjODU5Nzk1YzFjYTZmOGJhMWJkMGM2NzFlNCIsImlhdCI6MTcwNjc3NzcxN30.zl7l97HNGu3O7TfAHHUIlIEwQQlvgkwYZ-XkmK3eOnw' });
+// const projectId = '2JRgSlpcFAFTDTvmS9ntQqfFkUZ';
+// const projectSecret = '29ffe1fe5dee36c49c82a6ad045c3259';
+// const auth =
+    // 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
+// const ipfs = new ipfsClient({
+//     host: 'api.pinata.cloud/pinning/pinFileToIPFS',//'ipfs.infura.io',
+//     port: 443,
+//     protocol: 'https',
+//     headers: {
+//        // 'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
+//         'Authorization': `Bearer ${"26b391f18a0b6c56e670"}`
+//       }})
+
+
 const app = express()
 app.use(cors({origin: '*'}));
 app.set('view engine','ejs')
@@ -33,7 +38,6 @@ app.post('/upload', (req, res) => {
      const file = req.files.file
      const fileName = req.body.filename
      const filePath =   './files/' + fileName
-     console.log(filePath);
      file.mv(filePath, async (err) => {
          if (err) {
             console.log(err)
@@ -41,21 +45,39 @@ app.post('/upload', (req, res) => {
             return res.status(500).send(err)
          }
      const fileHash = await addFile(fileName, filePath)
-     fs.unlink(filePath, (err) =>{
-         if (err) console.log(err)
-     })  
      res.render('upload', {fileName, fileHash})
      })
 })
 
 const addFile = async (fileName, filePath) => {
-    const file = fs.readFileSync(filePath)
-   const fileAdded = await ipfs.add({path:fileName, content:file},{ wrapWithDirectory: true })
-  
-
-
-   const fileHash = fileAdded.cid
-  console.log(fileAdded)
+    // const file = fs.readFileSync(filePath)
+    const stream = fs.createReadStream(filePath)
+    let options = {
+        pinataMetadata: {
+            name: fileName
+        }
+    }
+    if (filePath.substr(filePath.length - 4) !== ".png")
+    {
+        options = {
+            pinataMetadata: {
+                name: fileName
+            },
+            pinataOptions: {
+                cidVersion: 0, 
+                wrapWithDirectory: true
+            }
+        }
+    }
+//    const fileAdded = await ipfs.add({path:fileName, content:file},{ wrapWithDirectory: true })
+    const fileAdded = await pinata.pinFileToIPFS(stream, options)
+      
+    const fileHash = fileAdded.IpfsHash
+    
+    fs.unlinkSync(filePath)
+    console.log(fileAdded)
+    console.log(fileHash)
+    
     return fileHash
 }
 
